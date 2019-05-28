ad66051671967275fafcacd98d185e1b1d0dbb26
sergeyokten <sergey.okten@gmail.com>
2019-05-28 21:45:33 +0300

    update main comtrooler add comment

--- src/main/java/com/oktenweb/springbootlesson1/controllers/MainController.java
+++ src/main/java/com/oktenweb/springbootlesson1/controllers/MainController.java
@@ -15,7 +15,7 @@ import java.util.List;
 @AllArgsConstructor
 public class MainController {
 
-
+//jsahfdhsagfdhagfdhgafsdhgafsd
     private UserService userService;
 
     //http://localhost:8080/   [get]
